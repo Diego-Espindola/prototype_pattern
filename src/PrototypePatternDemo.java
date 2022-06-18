bb07b9342ba838f15918397d06541ddc82ea13fa
diego-espindola <diegoespindola9404@gmail.com>
2022-06-18 16:29:26 -0300

    Project finished and tested.

--- src/PrototypePatternDemo.java
+++ src/PrototypePatternDemo.java
@@ -1,5 +1,17 @@
+import com.prototypepattern.Shape;
+import com.prototypepattern.ShapeCache;
+
 public class PrototypePatternDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape: " + clonedShape.getType());;
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape: " + clonedShape2.getType());;
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape: " + clonedShape3.getType());;
     }
 }
