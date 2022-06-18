727d56ea429b16818cc7dca2fc4f70193b532eae
diego-espindola <diegoespindola9404@gmail.com>
2022-06-18 16:16:35 -0300

    Finished Rectangle.java

--- src/com/prototypepattern/Rectangle.java
+++ src/com/prototypepattern/Rectangle.java
@@ -1,4 +1,11 @@
 package com.prototypepattern;
 
 public class Rectangle extends Shape{
+    public Rectangle(){
+        type = "Rectangle";
+    }
+    @Override
+    public void draw(){
+        System.out.println("Inside Rectangle::draw() method.");
+    }
 }
